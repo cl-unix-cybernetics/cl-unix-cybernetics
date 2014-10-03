0b63a1c88767ffb1b7323302505d789008defb3b
Thomas de Grivel <thomas@lowh.net>
2014-10-03 11:44:24 +0200

    Update tests

--- test.lisp
+++ test.lisp
@@ -3,17 +3,32 @@
 
 (require :adams)
 
-(use-package :adams)
+(in-package :adams-user)
 
 ;; TEST
 
 #+nil
 (untrace shell-status)
 
-(setf *debug* '(shell))
+(setf (debug-p :shell) t)
 
-(with-shell (shell)
-  (run-command "hostname && false" shell)
-  (run-command "pwd" shell)
-  (run-command "ls" shell)
-  (run-command "exit" shell))
+(with-host "h"
+  (run "hostname && false")
+  (run "pwd")
+  (run "ls")
+  (run "exit"))
+
+(with-manifest "h"
+  (make-instance 'user :name "vmail"
+		 :shell "/bin/ksh"
+		 :home "/var/qmail/domains"
+		 :gid 13000
+		 :uid 13000))
+
+(adams::apply-manifest "h")
+
+(manifest-resources (manifest "h"))
+
+(apply-manifest "h")
+
+(remove-manifest "h")
