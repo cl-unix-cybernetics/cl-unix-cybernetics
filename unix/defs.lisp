4424310c6f32528a2abe02eda57c84439302b601
Thomas de Grivel <thomas@lowh.net>
2015-08-01 01:42:16 +0200

    op-chown

--- unix/defs.lisp
+++ unix/defs.lisp
@@ -43,10 +43,12 @@
   ((probe-vnode-using-ls :properties (:mode :links :owner :group :size :mtime))
    (probe-vnode-using-stat :properties (:dev :ino :mode :links :uid :gid :rdev
                                         :size :atime :mtime :ctime :blksize
-                                        :blocks :flags))))
+                                        :blocks :flags)))
+  ((op-chown :properties (:uid :gid :owner :group))))
 
 (defgeneric probe-vnode-using-ls (resource os))
 (defgeneric probe-vnode-using-stat (resource os))
+(defgeneric op-chown (resource os &key uid gid owner group &allow-other-keys))
 
 ;;  File
 
